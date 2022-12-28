c51fbdd415a6903d71ab653e64a1d62522cb0378
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-12-29 03:43:17 +0500

    App page setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import useMediaQuery from './hooks/useMediaQuery';
+import Navbar from "./scenes/Navbar";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
-    <></>
+    <div className='app bg-deep-blue'>
+      <Navbar 
+        selectedPage = {selectedPage}
+        setSelectedPage = {setSelectedPage}
+      />
+    </div>
   );
 }
 
