1ee02ef39bc366626577fbd6dd531bafc336e402
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-12-31 01:50:27 +0500

    Landing section CTA

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -44,6 +44,21 @@ const Landing = ({ setSelectedPage }) => {
 Transforming ideas into reality with cutting-edge engineering
                         </p>
                     </motion.div>
+
+                    {/* Call to actions */}
+                    <motion.div
+                       className="flex mt-5 justify-center md:justify-start"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{once:true, amount:0.5}}
+                        transition={{delay:0.2, duration:0.5}}
+                        variants={{
+                            hidden:{opacity:0, x:-50},
+                            visible:{opacity:1, x:0}
+                        }}
+                    >
+
+                    </motion.div>
             </div>
         </section>
     )
