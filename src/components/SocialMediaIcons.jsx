29e50acbc12f07ee7ed7f356412022186e0e94a9
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-12-31 02:38:01 +0500

    Social Media Links added

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
 const SocialMediaIcons = () => {
-  return (
-    <div>SocialMediaIcons</div>
-  )
-}
+	return (
+		<div className="flex justify-center md:justify-start my-10 gap-7">
+			<a
+				href="https://www.linkedin.com/in/ahmed-bashir-880133129/"
+				target="_blank"
+				rel="noreferrer"
+				className="hover:opacity-50 transition duration-500"
+			>
+				<img alt="linkedin-ahmed-bashir" src="../assets/linkedin.png" />
+			</a>
 
-export default SocialMediaIcons
+			<a
+				href="https://twitter.com/ahmedbashir93"
+				target="_blank"
+				rel="noreferrer"
+				className="hover:opacity-50 transition duration-500"
+			>
+				<img alt="twitter-ahmed-bashir" src="../assets/twitter.png" />
+			</a>
+
+			<a
+				href="https://web.facebook.com/ahmad.bashir.796/"
+				target="_blank"
+				rel="noreferrer"
+				className="hover:opacity-50 transition duration-500"
+			>
+				<img alt="facebook-ahmed-bashir" src="../assets/facebook.png" />
+			</a>
+
+			<a
+				href="https://www.instagram.com/ahmed1139/"
+				target="_blank"
+				rel="noreferrer"
+				className="hover:opacity-50 transition duration-500"
+			>
+				<img alt="instagram-ahmed-bashir" src="../assets/instagram.png" />
+			</a>
+		</div>
+	);
+};
+
+export default SocialMediaIcons;
