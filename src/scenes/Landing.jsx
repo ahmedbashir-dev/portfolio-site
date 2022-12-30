c5d5265a49b10b953ec63c9a84c841b251e54d6b
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-12-31 02:29:21 +0500

    Social Media Icons motion

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,6 +1,7 @@
-import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+import SocialMediaIcons from "../components/SocialMediaIcons";
 const Landing = ({ setSelectedPage }) => {
 	const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
 
@@ -87,12 +88,29 @@ const Landing = ({ setSelectedPage }) => {
 						onClick={() => setSelectedPage("contact")}
 						href="#contact"
 					>
-                        <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center
-                        justify-center font-playfair px-10">
-                            Let's talk
-                        </div>
-                    </AnchorLink>
+						<div
+							className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center
+                        justify-center font-playfair px-10"
+						>
+							Let's talk
+						</div>
+					</AnchorLink>
 				</motion.div>
+
+                <motion.div
+					className="flex mt-5 justify-center md:justify-start"
+					initial="hidden"
+					whileInView="visible"
+					viewport={{ once: true, amount: 0.5 }}
+					transition={{ delay: 0.2, duration: 0.5 }}
+					variants={{
+						hidden: { opacity: 0, x: -50 },
+						visible: { opacity: 1, x: 0 },
+					}}
+				>
+
+                    <SocialMediaIcons />
+                </motion.div>
 			</div>
 		</section>
 	);
