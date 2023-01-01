ea6b663fabef46deb853fb171fdf8eaf1f448837
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-01-01 14:33:18 +0500

    String resources added

---
+++ src/res/strings.js
@@ -0,0 +1,11 @@
+export const strings = {
+    'en':{
+        "experience-section": `In addition to my career as a software engineer at PrescribeNow Inc., 
+        where I have contributed to the development of successful product using technologies 
+        such as React JS and Spring Boot, I also have experience as a teacher. 
+        I have taught programming and web development related courses to students at the GIFT University for the past 3 years, 
+        where I have developed and led 6+ courses. My students consistently give me high ratings 
+        for my ability to explain complex concepts in a clear and engaging manner. 
+        I am passionate about sharing my knowledge and helping others learn and grow.`
+    }
+}
