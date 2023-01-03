2e092a4cff872b971740e220a706442ada35493f
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-01-03 22:09:51 +0500

    projects added

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -82,7 +82,22 @@ const Projects = ({ language }) => {
             BEAUTIFUL USER INTERFACES
           </div>
           <Project title="Project 1" />
-          
+          <Project title="Project 2" />
+
+          {/* ROW 2 */}
+          <Project title="Project 3" />
+          <Project title="Project 4" />
+          <Project title="Project 5" />
+
+          {/* ROW 3 */}
+          <Project title="Project 6" />
+          <Project title="Project 7" />
+          <div
+            className="flex justify-center text-center items-center p-10 bg-blue
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            SMOOTH USER EXPERIENCE
+          </div>
         </motion.div>
 			</div>
 		</section>
